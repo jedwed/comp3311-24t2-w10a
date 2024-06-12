1e25df85c983bb2bc4a2c65e4e043928d9a246e2
jedwed <83894732+jedwed@users.noreply.github.com>
2024-06-12 12:06:31 +1000

    Push Q15 code

---
+++ tut03/Q15.sql
@@ -0,0 +1,38 @@
+CREATE TABLE players (
+    name text PRIMARY KEY
+);
+
+CREATE TABLE teams (
+    name text PRIMARY KEY,
+    captain text REFERENCES players (name)
+);
+
+ALTER TABLE players ADD COLUMN playsFor text REFERENCES teams (name);
+
+CREATE TABLE fans (
+    name text PRIMARY KEY
+);
+
+CREATE TABLE teamColors (
+    team text REFERENCES teams (name),
+    color text,
+    PRIMARY KEY (team, color)
+);
+
+CREATE TABLE fanFavColors (
+    fan text REFERENCES fans (name),
+    color text,
+    PRIMARY KEY (fan, color)
+);
+
+CREATE TABLE favPlayers (
+    fan text REFERENCES fans (name),
+    player text REFERENCES players (name),
+    PRIMARY KEY (fan, player)
+);
+
+CREATE TABLE favTeams (
+    fan text REFERENCES fans (name),
+    team text REFERENCES teams (name),
+    PRIMARY KEY (fan, team)
+);
