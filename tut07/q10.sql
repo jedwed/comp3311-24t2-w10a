65390a0f4bc99029aeb5acf03207345cd697942c
jedwed <83894732+jedwed@users.noreply.github.com>
2024-07-10 17:33:57 +1000

    Push tut07 content

--- tut07/q10.sql
+++ tut07/q10.sql
@@ -19,3 +19,55 @@ CREATE TABLE enrolment (
     FOREIGN KEY (course) REFERENCES Course(code),
     FOREIGN KEY (sid) REFERENCES Student(sid)
 );
+
+-- sample data for testing
+insert into Course values ('COMP1511', 'Jake Renzella', 1000);
+insert into Course values ('COMP1531', 'Hayden Smith' , 1000);
+insert into Course values ('COMP3311', 'Yuekang Li'   , 1000);
+
+insert into Student values (0, 'John Smith'   );
+insert into Student values (1, 'John Doe'     );
+insert into Student values (2, 'Daniel Jacobs');
+
+
+CREATE OR REPLACE FUNCTION insert_num_students() RETURNS trigger AS $$
+BEGIN
+    -- Increment course's numStudents
+    UPDATE 
+        course 
+    SET
+        numStudents = numStudents + 1 
+    WHERE
+        code = NEW.course;
+    RETURN NEW;
+END
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER insert_num_students
+    AFTER INSERT 
+    ON enrolment
+    FOR EACH ROW
+    EXECUTE FUNCTION insert_num_students();
+
+-- Define ASSERTION to ensure numStudents in each course = number of enrolments for that course
+
+-- CREATE ASSERTION num_students CHECK (
+--     -- Check there doesn't exist a single tuple where numStudents != number of enrolments for that course
+--     NOT EXISTS (
+--         -- Courses where numStudents != number of enrolments
+--         SELECT
+--             *
+--         FROM
+--             course AS c
+--         WHERE
+--             numStudents != (
+--                 -- Number of enrolments for the course
+--                 SELECT 
+--                     COUNT(*)
+--                 FROM
+--                     enrolments
+--                 WHERE
+--                     course = c.code
+--             )
+--     )
+-- )
--- tut07/q6.sql
+++ tut07/q6.sql
@@ -4,3 +4,38 @@ CREATE TABLE R(
     c text
     -- we want primary key (a, b)
 );
+
+
+CREATE OR REPLACE FUNCTION check_primary_key() RETURNS trigger AS $$
+BEGIN
+    IF NEW.a IS NULL OR NEW.b IS NULL THEN
+        RAISE EXCEPTION 'no no null primary key';
+    END IF;
+
+    -- Uniqueness
+
+    -- Edge case: if we try to update a tuple without changing the primary key
+    IF OLD.a = NEW.a AND OLD.b = NEW.b THEN
+        RETURN NEW;
+    END IF;
+
+    PERFORM 
+        *
+    FROM
+        R
+    WHERE
+        a = NEW.a AND b = NEW.b;
+
+    IF found THEN
+        RAISE EXCEPTION 'ruh roh primary key already exists';
+    END IF; 
+
+    RETURN NEW;
+END
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER primary_key_constraint
+    BEFORE INSERT OR UPDATE
+    ON R
+    FOR EACH ROW
+    EXECUTE FUNCTION check_primary_key();
